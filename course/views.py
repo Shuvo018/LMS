1c45ebcf7cda60e152b5c064e0654164bb20872a
Shuvo dev nath <shuvodevnath0188@gmail.com>
2026-07-18 00:41:02 +0600

    add course list view

--- course/views.py
+++ course/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from course.models import Course,Lesson,SupportingDoc,Quiz
+from course.serializers import (
+    CourseSerializer,
+    LessonSerializer,
+    SupportingDocSerializer,
+    QuizSerializer
+)
+from course.permissions import IsInstructorOnly
+from rest_framework.permissions import IsAuthenticated,AllowAny
 
-# Create your views here.
+
+class CourseListView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self,request):
+        try:
+            courses = Course.objects.all()
+            serializer = CourseSerializer(courses,many=True)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({"error":str(e)},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
--- lms/urls.py
+++ lms/urls.py
@@ -17,10 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from accounts.views import RegistrationView, LoginView, Logout, ForgotPasswordView
+from course.views import (
+    CourseListView
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', RegistrationView.as_view()),
     path('login/', LoginView.as_view()),
     path('logout/', Logout.as_view()),
     path('forgot_password/', ForgotPasswordView.as_view()),
+
+    path('courses/',view=CourseListView.as_view()),
 ]
